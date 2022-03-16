2014bc88759355fa588246c0a6f1be2201b46f7f
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-16 16:25:59 +0100

    adding template user route

---
+++ enedis_back/routes/users.routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const users = require('../models/users.model');
+
+router.get('/',async (res,req)=>{
+
+});
+
+router.get('/:id',async (res,req)=>{
+
+});
+
+router.post('/',async (res,req)=>{
+
+});
+
+router.put('/',async (res,req)=>{
+
+});
+
+router.delete('/',async (res,req)=>{
+
+});
+module.exports =router;
