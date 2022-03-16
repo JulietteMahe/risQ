ff6f06b652094dab7d624b4b9d006cf674b3aa9e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-16 11:24:43 +0100

    adding back end

---
+++ enedis_back/app.js
@@ -0,0 +1,38 @@
+const connection = require('./db-config');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = require('./routes/index.routes');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 8000;
+
+const corsOptions ={
+  origin:process.env.CLIENT_URL, 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+connection.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+    } else {
+      console.log('connected as id ' + connection.threadId);
+    }
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(cors(corsOptions));
+app.use('/api', router);
+
+app.get("/", (req, res) => {
+    res.send("Welcome");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+  
+  module.exports = app;
