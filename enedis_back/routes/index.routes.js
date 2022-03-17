8a2ebc4b9e138c3e58a3590104f89ce22e8af7aa
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-17 16:43:35 +0100

    adding state route

--- enedis_back/routes/index.routes.js
+++ enedis_back/routes/index.routes.js
@@ -3,10 +3,12 @@ const users=require('./users.routes');
 const auth=require('./auth.routes');
 const problemType=require('./problemType.routes');
 const problems=require('./problems.routes');
+const stateProblem=require('./state.routes');
 
 router.use('/users',users);
 router.use('/auth',auth);
 router.use('/type',problemType);
 router.use('/problem',problems);
+router.use('/state',stateProblem);
 
 module.exports = router;
