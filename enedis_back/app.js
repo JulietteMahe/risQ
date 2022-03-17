50cf9657bf9b2642b89bd299c081d695f753d11f
sam-rached <rachedsamir@gmail.com>
2022-03-17 22:25:10 +0100

    update

--- enedis_back/app.js
+++ enedis_back/app.js
@@ -1,40 +1,40 @@
-const connection = require('./db-config');
-const express = require('express');
-const cors = require('cors');
+const connection = require("./db-config");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const router = require('./routes/index.routes');
-const cookieParser = require('cookie-parser');
+const router = require("./routes/index.routes");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 8000;
 
-const corsOptions ={
-  origin:process.env.CLIENT_URL, 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 connection.connect((err) => {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-    } else {
-      console.log('connected as id ' + connection.threadId);
-    }
-  });
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  } else {
+    console.log("connected as id " + connection.threadId);
+  }
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors(corsOptions));
-app.use('/api', router);
+// app.use(cors(corsOptions));
+app.use("/api", router);
 
-app.use('/uploads',express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
-    res.send("Welcome");
+  res.send("Welcome");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
-  
-  module.exports = app;
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
