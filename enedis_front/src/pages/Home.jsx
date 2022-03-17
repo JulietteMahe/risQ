c0e5baa5b6423b1da1dca730d4dfaece68fa5936
mentat51 <sylvain.fauveau@gmail.com>
2022-03-17 17:41:31 +0100

    fixes 2

--- enedis_front/src/pages/Home.jsx
+++ enedis_front/src/pages/Home.jsx
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-import { Link } from "react-router-dom";
-import Geo from "../components/Geo";
-import Navbar from "../components/Navbar";
-import "./Home.css";
-
-const Home = () => {
-  return (
-    <div className="home">
-      <Link to="/camera1">test photo1</Link>
-      <Link to="/camera2">test photo2</Link>
-      <Link to="/camera3">test photo3</Link>
-      <Link to="/camera4">test photo4</Link>
-      <Geo />
-    </div>
-  );
-};
-=======
 
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Geo from '../components/Geo';
-import Navbar from '../components/Navbar';
+// import Navbar from '../components/Navbar';
 import Fondamentaux from './Fondamentaux.jsx';
 import './Home.css';
 
@@ -35,10 +16,10 @@ const Home = () => {
             <li><Link to="/camera2">test photo2</Link></li>
             <li><Link to="/camera3">test photo3</Link></li>
             <li><Link to="/osmap">test map</Link></li>
+            <li><Link to="/camera4">test photo4</Link></li>
             </ul>
         </div>
     )
 }
->>>>>>> 7362dcde2a1a945768ede231aade8f77fd1388e0
 
 export default Home;
