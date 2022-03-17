a270e79e889880e103b26b05789644c182057fd1
Romain JUGAL <romain-jugal_student2021@wilder.school>
2022-03-17 15:08:27 +0100

    fondamentals component

---
+++ enedis_front/src/pages/Fondamentaux.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Fondamentaux.css"
+
+const Fondamentaux = () => {
+    return (
+        <img src="https://zupimages.net/up/22/11/n5tj.jpg" alt="Les 8 fondamentaux d'Enedis" className='imageFondamentaux' />
+    )
+}
+
+export default Fondamentaux;
--- enedis_front/src/pages/Home.jsx
+++ enedis_front/src/pages/Home.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Geo from '../components/Geo';
+import Fondamentaux from './Fondamentaux.jsx';
 import './Home.css';
 
 const Home = () => {
     return (
         <div>
+            <Fondamentaux />
             <Geo />
             <Link to="/camera1">test photo1</Link>
             <Link to="/camera2">test photo2</Link>
